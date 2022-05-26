91190b0bc7c8df858b435eea5afdd8d779b5a180
Robert Haidl <haidlrob@gmail.com>
2022-05-26 19:10:19 +0200

    updated url

--- src/app/_services/restaurant.service.ts
+++ src/app/_services/restaurant.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const AUTH_API = "https://restaurants-austria-backend/api/restaurants/";
+const AUTH_API = "https://restaurants-austria-backend.herokuapp.com/api/restaurants/";
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' , 'Authorization': 'OKLwnfj348NHFnj', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT', 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'})
 };
